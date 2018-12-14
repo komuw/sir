0883bcdadfccf0cfb5f770445a3a066cf151c46c
komuW <komuw05@gmail.com>
2018-12-14 12:31:33 +0300

    proxies

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/komuw/dbscan
+
+require github.com/pkg/errors v0.8.0
--- reverse.go
+++ reverse.go
@@ -1,54 +1,71 @@
 package main
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net"
+	"os"
+
+	"github.com/komuw/dbscan/proxyd"
+	"github.com/pkg/errors"
 )
 
 /*
 usage:
-  go run -race reverse.go
-  echo -n "test out the server" | nc localhost 3333
-  curl -vkIL localhost:3333
+  go run -race reverse.go localhost:7777 localhost:3333
+  curl -vkL localhost:7777
+  echo -n "test out the server" | nc localhost 7777
 */
 
-const (
-	connHost = "localhost"
-	connPort = "3333"
-	connType = "tcp"
-)
-
-func main() {
-	l, err := net.Listen(connType, connHost+":"+connPort)
-	if err != nil {
-		log.Fatal("Error listening:", err)
-	}
-	defer l.Close()
-
-	log.Println("Listening on " + connHost + ":" + connPort)
-	for {
-		// Listen for an incoming connection.
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal("Error accepting:", err)
-		}
-		go handleRequest(conn)
-	}
-}
-
-func handleRequest(conn net.Conn) {
+func forward(conn net.Conn) {
 	defer conn.Close()
 
-	// TODO: make the buffer growable
 	buf := make([]byte, 100)
 	reqLen, err := conn.Read(buf)
 	if err != nil {
-		log.Fatal("Error reading:", err)
+		err = errors.Wrap(err, "Reverse Error reading")
+		log.Fatalf("%+v", err)
 	}
 	_ = reqLen
+	log.Println("Reverse read::", buf)
+	log.Println("Reverse read2::", string(buf))
 
-	log.Println("received::", buf)
-	log.Println("received2::", string(buf))
+	client, err := net.Dial("tcp", os.Args[2])
+	if err != nil {
+		err = errors.Wrap(err, "Reverse Dial failed")
+		log.Fatalf("%+v", err)
+	}
+	defer client.Close()
+	log.Printf("Connected to localhost %v\n", conn)
 
-	conn.Write([]byte("Message received."))
+	io.Copy(client, bytes.NewReader(buf))
+	io.Copy(conn, client)
+}
+
+func main() {
+	{
+		go proxyd.Run()
+	}
+
+	if len(os.Args) != 3 {
+		log.Fatalf("Reverse Usage %+v listen:port forward:port\n", os.Args[0])
+		return
+	}
+
+	listener, err := net.Listen("tcp", os.Args[1])
+	if err != nil {
+		err = errors.Wrapf(err, "Reverse failed to setup listener %v", os.Args[1])
+		log.Fatalf("%+v", err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			err = errors.Wrapf(err, "Reverse failed to accept listener %v", os.Args[1])
+			log.Fatalf("%+v", err)
+		}
+		log.Printf("Accepted connection %v\n", conn)
+		go forward(conn)
+	}
 }
