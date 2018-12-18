ddeecac87fdb2fb4cd9a7e1c879dd5927646b7da
komuW <komuw05@gmail.com>
2018-12-18 15:19:28 +0300

    test

--- go_dbscan/go_dbscan.go
+++ go_dbscan/go_dbscan.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"flag"
 	"fmt"
 	"image/color"
 	"log"
@@ -44,29 +43,10 @@ func findClusterMembers(labels []int, X *mat.Dense) error {
 		}
 
 	}
-
-	// cluster_members =  map[string]int    //{"noise": []}
-	// for val in labels:
-	//     if val != -1:
-	//         cluster_members.update({str(val): []})
-
-	// for k, v in enumerate(labels):
-	//     if v == -1:
-	//         # noise
-	//         cluster_members["noise"].append(X[k])
-	//     for cluster_members_key in cluster_members.keys():
-	//         if str(v) == cluster_members_key:
-	//             cluster_members[cluster_members_key].append(X[k])
 	return nil
 }
 
-var visualDebug = flag.Bool("visual", false, "output images for benchmarks and test data")
-
 func main() {
-	*visualDebug = true
-	// Save the plot to a PNG file.
-	pngfile := "GolangExampleDBSCAN.png"
-
 	// adapted from http://scikit-learn.org/stable/_downloads/plot_dbscan.ipynb
 	// Generate sample data
 	centers := mat.NewDense(3, 2, []float64{1, 1, -1, -1, 1, -1})
@@ -92,52 +72,62 @@ func main() {
 	}
 	fmt.Printf("Estimated number of clusters: %d\n", nclusters)
 
-	findClusterMembers(labels, X)
-
-	if *visualDebug {
-
-		// plot result
-		p, err := plot.New()
-		if err != nil {
-			err = errors.Wrap(err, "error instantiating plot")
-			log.Fatalf("%+v", err)
-
-		}
-		p.Title.Text = fmt.Sprintf("Estimated number of clusters: %d", nclusters)
-		for cl := range labelsmap {
-			var data plotter.XYs
-			for sample := 0; sample < NSamples; sample++ {
-				if labels[sample] == cl {
-					data = append(data, struct{ X, Y float64 }{X.At(sample, 0), X.At(sample, 1)})
-				}
-			}
-			s, err := plotter.NewScatter(data)
-			if err != nil {
-				err = errors.Wrap(err, "error instantiating plotter.NewScatter")
-				log.Fatalf("%+v", err)
-			}
-			var color0 color.RGBA
-			switch cl {
-			case -1:
-				color0 = color.RGBA{0, 0, 0, 255}
-			case 0:
-				color0 = color.RGBA{176, 0, 0, 255}
-			case 1:
-				color0 = color.RGBA{0, 176, 0, 255}
-			case 2:
-				color0 = color.RGBA{0, 0, 176, 255}
-			}
-			s.GlyphStyle.Color = color0
-			s.GlyphStyle.Shape = draw.CircleGlyph{}
-			p.Add(s)
-			// p.Legend.Add(fmt.Sprintf("scatter %d", cl), s)
-
-		}
-
-		if err := p.Save(6*vg.Inch, 4*vg.Inch, pngfile); err != nil {
-			err = errors.Wrap(err, "error saving png")
-			log.Fatalf("%+v", err)
-		}
+	err := plotResults(labelsmap, NSamples, labels, nclusters, X)
+	if err != nil {
+		log.Fatalf("%+v", err)
 
 	}
+	err = findClusterMembers(labels, X)
+	if err != nil {
+		log.Fatalf("%+v", err)
+
+	}
+
+}
+
+func plotResults(labelsmap map[int]int, NSamples int, labels []int, nclusters int, X *mat.Dense) error {
+	// Save the plot to a PNG file.
+	pngfile := "GolangExampleDBSCAN.png"
+
+	// plot result
+	p, err := plot.New()
+	if err != nil {
+		return errors.Wrap(err, "error instantiating plot")
+
+	}
+	p.Title.Text = fmt.Sprintf("Estimated number of clusters: %d", nclusters)
+	for cl := range labelsmap {
+		var data plotter.XYs
+		for sample := 0; sample < NSamples; sample++ {
+			if labels[sample] == cl {
+				data = append(data, struct{ X, Y float64 }{X.At(sample, 0), X.At(sample, 1)})
+			}
+		}
+		s, err := plotter.NewScatter(data)
+		if err != nil {
+			return errors.Wrap(err, "error instantiating plotter.NewScatter")
+		}
+		var color0 color.RGBA
+		switch cl {
+		case -1:
+			color0 = color.RGBA{0, 0, 0, 255}
+		case 0:
+			color0 = color.RGBA{176, 0, 0, 255}
+		case 1:
+			color0 = color.RGBA{0, 176, 0, 255}
+		case 2:
+			color0 = color.RGBA{0, 0, 176, 255}
+		}
+		s.GlyphStyle.Color = color0
+		s.GlyphStyle.Shape = draw.CircleGlyph{}
+		p.Add(s)
+		// p.Legend.Add(fmt.Sprintf("scatter %d", cl), s)
+
+	}
+
+	if err := p.Save(6*vg.Inch, 4*vg.Inch, pngfile); err != nil {
+		return errors.Wrap(err, "error saving png")
+	}
+
+	return nil
 }
