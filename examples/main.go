727f2d57796b4e537b42d3ba0053e6f708b1c72f
komuW <komuw05@gmail.com>
2018-12-31 16:01:09 +0300

    ff

--- examples/main.go
+++ examples/main.go
@@ -6,8 +6,18 @@ import (
 	"io/ioutil"
 	"log"
 	"net"
+	"sync"
 	"time"
+
+	"github.com/komuw/dbscan/heart"
+	"github.com/pkg/errors"
 )
+
+func cooler() {
+	thisStruct.l.Lock()
+	defer thisStruct.l.Unlock()
+	heart.Run(thisStruct.noOfAllRequests, thisStruct.lengthOfEachRequest, thisStruct.allRequests, 3.0, 1.0, false)
+}
 
 func main() {
 	/*
@@ -28,6 +38,10 @@ func main() {
 	    curl -vL -H "Host: httpbin.org" localhost:7777/get
 	`)
 
+	{
+		time.AfterFunc(23*time.Second, cooler)
+	}
+
 	for {
 		frontendConn, err := listener.Accept()
 		if err != nil {
@@ -39,32 +53,59 @@ func main() {
 	}
 }
 
+type myStruct struct {
+	l                   sync.RWMutex
+	noOfAllRequests     int
+	allRequests         []float64
+	lengthOfEachRequest int
+}
+
+var thisStruct myStruct
+
+func handleRequest(requestBuf []byte) {
+	thisStruct.l.Lock()
+	defer thisStruct.l.Unlock()
+	thisStruct.lengthOfEachRequest = len(requestBuf)
+
+	for _, v := range requestBuf {
+		thisStruct.allRequests = append(thisStruct.allRequests, float64(v))
+	}
+}
+
+const nulByte = "\x00"
+
 func forward(frontendConn net.Conn, remoteAddr string) {
 	defer frontendConn.Close()
 	err := frontendConn.SetDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
-		log.Fatalf("Unable to set frontendConn deadline %v", err)
+		err = errors.Wrap(err, "unable to set reverseProxyConn deadline")
+		log.Fatalf("%+v", err)
 	}
 
+	//////////////////////////////////// LOG REQUEST ////////////////////////
 	// TODO: make the buffer growable
-	requestBuf := make([]byte, 512)
+	requestBuf := make([]byte, 1024, 1024)
 	reqLen, err := frontendConn.Read(requestBuf)
 	if err != nil {
 		log.Fatalf("Error reading %+v", err)
 	}
 	_ = reqLen
+	requestBuf = bytes.Trim(requestBuf, nulByte)
+	handleRequest(requestBuf)
 	log.Println("we sent request::", requestBuf)
 	log.Println("we sent request::", string(requestBuf))
+	//////////////////////////////////// LOG REQUEST ////////////////////////
 
 	backendConn, err := net.Dial("tcp", remoteAddr)
 	if err != nil {
-		msg := "Dial failed for address" + remoteAddr
-		log.Fatalf("%+v %+v", msg, err)
+		err = errors.Wrap(err, "dial failed for address"+remoteAddr)
+		log.Fatalf("%+v", err)
 	}
 	defer backendConn.Close()
 	err = backendConn.SetDeadline(time.Now().Add(5 * time.Second))
 	if err != nil {
-		log.Fatalf("Unable to set backendConn deadline %v", err)
+		err = errors.Wrap(err, "unable to set backendConn deadline")
+		log.Fatalf("%+v", err)
 	}
 	log.Print("frontendConnected")
 
@@ -73,11 +114,43 @@ func forward(frontendConn net.Conn, remoteAddr string) {
 	io.Copy(backendConn, bytes.NewReader(requestBuf))
 	io.Copy(frontendConn, backendTee)
 
+	//////////////////////////////////// LOG RESPONSE ////////////////////////
 	backendBytes, err := ioutil.ReadAll(&backendBuf)
 	if err != nil {
-
-		log.Fatalf("Unable to read backendBuf %+v", err)
+		err = errors.Wrap(err, "unable to read backendBuf")
+		log.Fatalf("%+v", err)
 	}
 	log.Println("we got response::", backendBytes)
 	log.Println("we got response::", string(backendBytes))
+	//////////////////////////////////// LOG RESPONSE ////////////////////////
+
+	thisStruct.l.Lock()
+	thisStruct.noOfAllRequests++
+	log.Println("allRequests:", thisStruct.allRequests)
+	log.Println("lengthOfEachRequest:", thisStruct.lengthOfEachRequest)
+	thisStruct.l.Unlock()
 }
+
+// Hello.
+// I'm trying to create a TCP reverse proxy.
+// It should work for both http/https/rabbitMQ etc, anything that talks tcp.
+
+// The way it should work is:
+// 1. A client sends requests to it.
+// 2. The reverse proxy logs the resquest(by reading it)
+// 3. The reverse proxy forwards the request to a backend
+// 4. When the backend responds, the reverse proxy logs the response(by reading it)
+// 5. The reverse proxy forwards the response to the client
+
+// I have two problems/questions.
+// 1. In the code to log request
+// `requestBuf := make([]byte, 512)`
+// how do I make the buffer growable?
+// I want to make it possible to handle a request of arbitrary size.
+
+// 2. Why does my reverse proxy fail to work for https?
+// ie, when I set
+// `backendAddr := "httpbin.org:443"`
+// the reverse proxy does not work anymore.
+
+// Thanks
