a0e64e6a94c263e980e5a118b6500564d4092363
komuW <komuw05@gmail.com>
2019-01-02 16:32:02 +0300

    rr

--- heart/heart.go
+++ heart/heart.go
@@ -1,22 +1,13 @@
 package heart
 
 import (
-	"fmt"
-	"image/color"
 	"log"
 
 	"github.com/pa-m/sklearn/cluster"
 	"github.com/pa-m/sklearn/datasets"
 	"github.com/pa-m/sklearn/preprocessing"
-	"gonum.org/v1/gonum/mat"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/vg"
-
-	"gonum.org/v1/plot/vg/draw"
-
 	"github.com/pkg/errors"
-	"time"
+	"gonum.org/v1/gonum/mat"
 )
 
 /*
@@ -93,11 +84,6 @@ func Run(noOfAllRequests int, lengthOfLargestRequest int, allRequests []float64,
 	}
 	log.Printf("Estimated number of clusters: %d\n", nclusters)
 
-	// err := PlotResults(labelsmap, noOfAllRequests, labels, nclusters, X, appendName)
-	// if err != nil {
-	// 	log.Fatalf("\n%+v", err)
-
-	// }
 	err := findClusterMembers(labels, X)
 	if err != nil {
 		log.Fatalf("\n%+v", err)
@@ -108,16 +94,7 @@ func Run(noOfAllRequests int, lengthOfLargestRequest int, allRequests []float64,
 	err = PlotResultsPCA(noOfAllRequests, proj, nclusters, appendName)
 	if err != nil {
 		log.Fatalf("\n%+v", err)
-
 	}
-
-	// if appendName == "Requests" {
-	// 	PlotHeatMap(noOfAllRequests, lengthOfLargestRequest, X, appendName)
-	// } else if appendName == "testHeartRun" {
-	// 	PlotHeatMap(noOfAllRequests, lengthOfLargestRequest, X, appendName)
-	// }
-	// TODO: we should be able to handle responses that are not of equal size.
-	// when we do so; PlotHeatMap should also be applied to responses
 }
 
 func getX(noOfAllRequests int, lengthOfLargestRequest int, allRequests []float64) *mat.Dense {
@@ -131,55 +108,4 @@ func generateSampleData(noOfAllRequests int) *mat.Dense {
 	X, _ = preprocessing.NewStandardScaler().FitTransform(X, nil)
 
 	return X
-}
-
-func PlotResults(labelsmap map[int]int, noOfAllRequests int, labels []int, nclusters int, X *mat.Dense, appendName string) error {
-	now := time.Now()
-	// Save the plot to a PNG file.
-	pngfile := appendName + "_" + now.Format("Jan_2_2006_15_04_05") + ".png"
-
-	// plot result
-	pt, err := plot.New()
-	if err != nil {
-		return errors.Wrap(err, "error instantiating plot")
-
-	}
-	pt.Add(plotter.NewGrid())
-	pt.Title.Text = fmt.Sprintf("Estimated number of clusters: %d", nclusters)
-
-	for cl := range labelsmap {
-		var data plotter.XYs
-		for sample := 0; sample < noOfAllRequests; sample++ {
-			if labels[sample] == cl {
-				data = append(data, struct{ X, Y float64 }{X.At(sample, 0), X.At(sample, 1)})
-			}
-		}
-		s, err := plotter.NewScatter(data)
-		if err != nil {
-			return errors.Wrap(err, "error instantiating plotter.NewScatter")
-		}
-		var color0 color.RGBA
-		switch cl {
-		case -1:
-			color0 = color.RGBA{0, 0, 0, 255}
-		case 0:
-			color0 = color.RGBA{176, 0, 0, 255}
-		case 1:
-			color0 = color.RGBA{0, 176, 0, 255}
-		case 2:
-			color0 = color.RGBA{0, 0, 176, 255}
-		}
-		s.GlyphStyle.Color = color0
-		s.GlyphStyle.Shape = draw.CircleGlyph{}
-		pt.Add(s)
-		// p.Legend.Add(log.Sprintf("scatter %d", cl), s)
-
-	}
-
-	err = pt.Save(6*vg.Inch, 4*vg.Inch, pngfile)
-	if err != nil {
-		return errors.Wrap(err, "error saving png")
-	}
-
-	return nil
 }
