f027654df7d2d46b33f7cdc7849829d215781359
komuW <komuw05@gmail.com>
2019-01-02 14:01:57 +0300

    gg

--- heart/heart.go
+++ heart/heart.go
@@ -13,7 +13,6 @@ import (
 	"gonum.org/v1/plot/plotter"
 	"gonum.org/v1/plot/vg"
 
-	"gonum.org/v1/gonum/stat"
 	"gonum.org/v1/plot/vg/draw"
 
 	"github.com/pkg/errors"
@@ -106,7 +105,7 @@ func Run(noOfAllRequests int, lengthOfEachRequest int, allRequests []float64, Ep
 	}
 
 	proj := FindPCA(X, lengthOfEachRequest)
-	err = PlotResultsPCA(noOfAllRequests, proj)
+	err = PlotResultsPCA(noOfAllRequests, proj, nclusters, appendName)
 	if err != nil {
 		log.Fatalf("\n%+v", err)
 
@@ -183,60 +182,4 @@ func PlotResults(labelsmap map[int]int, noOfAllRequests int, labels []int, nclus
 	}
 
 	return nil
-}
-
-func PlotResultsPCA(noOfRequests int, X *mat.Dense) error {
-	now := time.Now()
-	// Save the plot to a PNG file.
-	pngfile := "komu" + "_" + now.Format("Jan_2_2006_15_04_05") + ".png"
-
-	// plot result
-	pt, err := plot.New()
-	if err != nil {
-		return errors.Wrap(err, "error instantiating plot")
-
-	}
-	pt.Add(plotter.NewGrid())
-	pt.Title.Text = fmt.Sprint("test standalone mat.Dense plot")
-
-	// type XYs []struct{ X, Y float64 }
-	var data plotter.XYs
-	for sample := 0; sample < noOfRequests; sample++ {
-		data = append(data, struct{ X, Y float64 }{X.At(sample, 0), X.At(sample, 1)})
-	}
-
-	s, err := plotter.NewScatter(data)
-	if err != nil {
-		return errors.Wrap(err, "error instantiating plotter.NewScatter")
-	}
-	var color0 color.RGBA
-	color0 = color.RGBA{176, 0, 0, 255}
-	s.GlyphStyle.Color = color0
-	s.GlyphStyle.Shape = draw.CircleGlyph{}
-	pt.Add(s)
-	// p.Legend.Add(log.Sprintf("scatter %d", cl), s)
-
-	err = pt.Save(6*vg.Inch, 4*vg.Inch, pngfile)
-	if err != nil {
-		return errors.Wrap(err, "error saving png")
-	}
-
-	return nil
-}
-
-func FindPCA(X *mat.Dense, d int) *mat.Dense {
-	// Calculate the principal component direction vectors
-	// and variances.
-	var pc stat.PC
-	ok := pc.PrincipalComponents(X, nil)
-	if !ok {
-		log.Fatal(errors.New("unable to get PrincipalComponents"))
-	}
-	log.Println("variances ", pc.VarsTo(nil))
-
-	k := 2
-	var proj mat.Dense
-	proj.Mul(X, pc.VectorsTo(nil).Slice(0, d, 0, k))
-
-	return &proj
 }
